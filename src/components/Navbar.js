dce9db668d6047f1f82e867db140e9590bcab01d
wesleyeagles <84718917+wesleyeagles@users.noreply.github.com>
2022-05-18 19:06:52 -0300

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import Logo from '../images/logoMariana.png'
 import { BsTwitter } from 'react-icons/bs'
@@ -7,13 +8,18 @@ import { BsInstagram } from 'react-icons/bs'
 import { BsLinkedin } from 'react-icons/bs'
 import { GiHamburgerMenu } from 'react-icons/gi'
 
-const menuMobile = document.getElementById('menu-mobile')
 
-function openMenu() {
-    menuMobile.classList.toggle('h-full')
-}
+
  
-const Navbar = () => {
+function Navbar(props) {
+
+    const [isActive, setActive] = useState(0);
+
+
+    const ToggleClass = () => {
+      setActive(!isActive); 
+     };
+
   return (
     <nav className='bg-black relative'>
         <div className='flex items-center justify-between md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto'>
@@ -33,14 +39,20 @@ const Navbar = () => {
             <Link to='/'> <BsLinkedin size={30} className='fill-pink-600 h-12 hover:fill-pink-300 transition-all'/>  </Link>
         </ul>
 
-        <ul id='menu-mobile' className='flex flex-col items-center w-full transition-all -z-10 top-32 h-0 overflow-hidden absolute lg:hidden'>
+        <ul id='menu-mobile' className={`flex flex-col gap-2 items-center w-full transition-all -z-10 top-24 h-0 overflow-hidden absolute lg:hidden bg-black ${isActive ? "h-60 pt-10" : null}`}>
            <Link to='/'><li className='text-pink-600 font-medium uppercase hover:text-pink-300 transition-all'>Sobre</li></Link>
            <Link to='/'><li className='text-pink-600 font-medium uppercase hover:text-pink-300 transition-all'>Portfólio</li></Link>
            <Link to='/'><li className='text-pink-600 font-medium uppercase hover:text-pink-300 transition-all'>Blog</li></Link>
-           <Link to='/'><li className='text-pink-600 font-medium uppercase hover:text-pink-300 transition-all'>Contato</li></Link> 
+           <Link to='/'><li className='text-pink-600 font-medium uppercase hover:text-pink-300 transition-all'>Contato</li></Link>
+           <div className='flex gap-2'>
+           <Link to='/'> <BsTwitter size={30} className='fill-pink-600 h-12 hover:fill-pink-300 transition-all'/>  </Link>
+            <Link to='/'> <BsFacebook size={30} className='fill-pink-600 h-12 hover:fill-pink-300 transition-all'/>  </Link>
+            <Link to='/'> <BsInstagram size={30} className='fill-pink-600 h-12 hover:fill-pink-300 transition-all'/>  </Link>
+            <Link to='/'> <BsLinkedin size={30} className='fill-pink-600 h-12 hover:fill-pink-300 transition-all'/>  </Link>
+           </div>
         </ul>
 
-        <div> <GiHamburgerMenu onClick={() => openMenu()} size={30} className='fill-pink-600 h-12 cursor-pointer hover:fill-pink-300 transition-all lg:hidden mr-8 sm:mr-0' />  </div>
+        <div> <GiHamburgerMenu onClick={ToggleClass} size={30} className='fill-pink-600 h-12 cursor-pointer hover:fill-pink-300 transition-all lg:hidden mr-8 sm:mr-0' />  </div>
         </div>
     </nav>
   )
